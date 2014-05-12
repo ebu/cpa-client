a4f7601cc2ec6ac57b0cd4d1289283f9554f3bf5
barroco <barroco@ebu.ch>
2014-05-13 00:18:34 +0200

    Update config files

--- webflow/config.test.js
+++ webflow/config.test.js
@@ -1,34 +1,31 @@
 "use strict";
 
-exports.db = {
-  type: 'sqlite',
+module.exports = {
+  db: {
+    type: 'sqlite',
 
-  // Database filename for SQLite
-  // filename: 'test.sqlite',
+    // Database filename for SQLite
+    // filename: 'test.sqlite',
 
-  // For debugging, log SQL statements to the console
-  debug: false
-};
-
-exports.authorization_provider = {
-  name:              "Example AP",
-  authorization_uri: "https://ap.example.com/authorized",
-  base_uri:          "https://ap.example.com",
-  modes:             ["client", "user"]
-};
-
-exports.service_provider = {
-  name:   "BBC1",
-  scope:  "https://sp.example.com/"
-};
-
-
-
-exports.identity_providers = {
-  github: {
-    enabled: true,
-    client_id: '-',
-    client_secret: '-',
-    callback_url: 'http://local.ebu.io:8000/auth/github/callback'
+    // For debugging, log SQL statements to the console
+    debug: false
+  },
+  cpa: {
+    client_id: 100,
+    client_secret: '123',
+    client_base_uri: 'https://client.example.com',
+    authorization_provider_uri: 'https://ap.example.com'
+  },
+  service_provider: {
+    name:   "BBC1",
+    scope:  "https://sp.example.com/"
+  },
+  identity_providers: {
+    github: {
+      enabled: true,
+      client_id: '-',
+      client_secret: '-',
+      callback_url: 'https://client.example.com/auth/github/callback'
+    }
   }
 };
