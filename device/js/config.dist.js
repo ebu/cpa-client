903ddf8a7239ed901b220b7d7b606651320d6db2
barroco <barroco@ebu.ch>
2014-05-24 00:43:48 +0200

    Replace scope by domain

--- device/js/config.dist.js
+++ device/js/config.dist.js
@@ -1,6 +1,6 @@
 var config = {
   //Should be dynamic using for instance /tokeninfo endpoint
-  scopes: {
+  domains: {
     'BBC1': 'http://local.ebu.io:8001/',
     'BBC2': 'http://local.ebu.io:8002/',
     'BBC3': 'http://local.ebu.io:8003/'
