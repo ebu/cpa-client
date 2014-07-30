17cbe299c65d650e87b65f5569a61fd43996f81a
barroco <barroco@ebu.ch>
2014-07-30 17:14:59 +0200

    Remove bower-installer dependency

--- device/js/config.dist.js
+++ device/js/config.dist.js
@@ -2,16 +2,24 @@ var config = {
   //Should be dynamic using for instance /tokeninfo endpoint
   domains: {
     'BBC1': {
-        domain: 'http://local.ebu.io:8001/',
-        id: '0.c221.ce15.ce1.dab'
+      domain: 'local.ebu.io:8000',
+      id: '0.c221.ce15.ce1.dab',
+      http: true
     },
     'BBC2': {
-        domain: 'http://local.ebu.io:8002/',
-        id: '0.c222.ce15.ce1.dab'
+      domain: 'local.ebu.io:8002',
+      id: '0.c222.ce15.ce1.dab',
+      http: true
     },
     'BBC3': {
-        // id will default to station name if omitted
-        domain: 'http://local.ebu.io:8003/'
+      domain: 'local.ebu.io:8003',
+      id: '0.c223.ce15.ce1.dab',
+      http: true
     },
+    'BBC4': {
+      domain: 'local.ebu.io:8004',
+      id: '0.c224.ce15.ce1.dab',
+      http: true
+    }
   }
 };
